3a80e2abab8cf675bb2ec7cae1766dd8b620a113
dragonstonehafiz <dragonstonehafiz@gmail.com>
2025-01-31 11:43:27 +0800

    added arduino project for esp32-cam

--- code_python/TEST_SerialMessaging.py
+++ code_python/TEST_SerialMessaging.py
@@ -1,16 +1,19 @@
 import serial.tools.list_ports
 
 if __name__ == "__main__":
-    # Load Serial Instance
-    serialInst = serial.Serial(port='COM4', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-    print(f"Serial Port is open: {serialInst.is_open}")
-    
-while 1:
-    command = input("Message to send (or quit): ")
-    if (command == "quit"):
-        break
-    
-    serialInst.write(command.encode("utf-8"))
-    
+    try:
+        # Load Serial Instance
+        serialInst = serial.Serial(port='COM4', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        print(f"Serial Port is open: {serialInst.is_open}")
+        
+        while 1:
+            command = input("Message to send (or quit): ")
+            if (command == "quit"):
+                break
+            
+            serialInst.write(command.encode("utf-8"))
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
     
     
