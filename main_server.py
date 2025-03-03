ad796e537606d7e835f4a5036814761a54d3c1b3
dragonstonehafiz <dragonstonehafiz@gmail.com>
2025-03-03 16:01:21 +0800

    update - added code to test sending and recieving images from pizero to desktop

---
+++ main_server.py
@@ -0,0 +1,16 @@
+from helper.PiCameraInterface import PiCameraInterface
+from helper.MQTT import MQTT_Publisher, MQTT_Subscriber
+from helper.utils import convert_bytes_to_frame
+import cv2
+
+def camera_data_callback(client, userdata, msg):
+    frame = convert_bytes_to_frame(msg.payload)
+    
+    # Display image
+    cv2.imshow("Received Image", frame)
+
+if __name__ == "__main__":
+    # The server will be the desktop/rasberry pi
+    mqtt_turn_data = MQTT_Publisher("localhost", "pizero/turn")
+    mqtt_image_data = MQTT_Subscriber("localhost", "pizero/image", camera_data_callback)
+     
