4991f08a32592ba110d18ad223579df656b47a38
hafiz <dragonstonehafiz@gmail.com>
2025-03-15 18:22:26 +0800

    update - onnx integer model

--- helper/YoloV5_ONNX.py
+++ helper/YoloV5_ONNX.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
-import onnxruntime
+import onnxruntime as ort
 import time
 
 class YoloV5_ONNX:
     def __init__(self, model_path="model/yolov5n_256.onnx"):
-        self.session = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+        so = ort.SessionOptions()
+        so.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL  # Enable all optimizations
+
+        self.session = ort.InferenceSession(model_path, sess_options=so, providers=["CPUExecutionProvider"])
         self.input_name = self.session.get_inputs()[0].name
         self.output_name = self.session.get_outputs()[0].name
         self.class_id = 14  # Bird class index (update if needed)
@@ -19,8 +22,6 @@ class YoloV5_ONNX:
     def detect_objects(self, image):
         """Runs inference and extracts (x, y, w, h, confidence, class_id)."""
         img_input = self.preprocess(image)
-
-        startTime = time.time()
         outputs = self.session.run([self.output_name], {self.input_name: img_input})[0]
 
         boxes, scores, class_ids = [], [], []
@@ -40,7 +41,7 @@ class YoloV5_ONNX:
         # Apply Non-Maximum Suppression (NMS)
         indices = cv2.dnn.NMSBoxes(boxes, scores, score_threshold=0.4, nms_threshold=0.5)
 
-        print(len(boxes))  # Print number of detected objects before NMS
+        # print(len(boxes))  # Print number of detected objects before NMS
 
         detections = []
         if len(indices) > 0:
@@ -50,10 +51,8 @@ class YoloV5_ONNX:
                 # x -= int(w / 2)
                 # y -= int(h / 2)
                 detections.append((x, y))
-                print(f"{x}, {y}")  # Print (x, y) as requested
+                # print(f"{x}, {y}")  # Print (x, y) as requested
 
-        endTime = time.time()
-        print(f"Inference Time {(endTime - startTime):0.2f}s")
 
         return detections
 
@@ -70,8 +69,18 @@ if __name__ == "__main__":
     image = cv2.imread(image_path)
     img_resized = cv2.resize(image, (256, 256))
 
-    model = YoloV5_ONNX("model/yolov5n_256.onnx")
+
+    model = YoloV5_ONNX("model/yolov5n_int.onnx")
+
+    startTime = time.time()
     objDetected = model.detect_objects(img_resized)
+    endTime = time.time()
+
+    elapsedTime = endTime - startTime
+    if elapsedTime < 1:
+        print(f"Inference Time {elapsedTime * 1000:0.2f}ms")
+    else:
+        print(f"Inference Time {elapsedTime:0.2f}s")
 
     # Render and show detections
     # image = model.render_detections(img_resized, objDetected)
